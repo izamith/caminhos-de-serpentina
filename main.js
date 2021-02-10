ab1e7523d951250a911b61ff86e27126e01aee0a
Isabela Zamith <izamith@yahoo.com.br>
2021-02-10 18:50:19 -0300

    Mapa básico + variáveis do mapa

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-function mapa() {
-// create map in div 'map'
-          var map = new maptalks.Map('map', {
-            center: [0, 0],
-            zoom: 2,
-            baseLayer: new maptalks.TileLayer('base', {
-              'urlTemplate' : 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-              'subdomains'  : ['a','b','c','d'],
-              'attribution'  : '&copy; <a href="http://www.osm.org/copyright">OSM</a> contributors, '+
-              '&copy; <a href="https://carto.com/attributions">CARTO</a>'
-            })
-          });
-        }
+var imagemMapa
+let xCoord = -122.4241
+let yCoord = 37.78
+let incline = 0 
+let mapHeigth = 512
+let mapWidth = 1024
+let accessToken = 'pk.eyJ1IjoicGlzY2luYWRlcGl4ZWwiLCJhIjoiY2trenk1ZzE2MGViYTJ1cG5hbXY1c3A5ZCJ9.lso-cNpB8Id_MW1s6_BM7A'
+
+function preload() {
+    imagemMapa = loadImage('https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/' + xCoord + ',' + yCoord + ',' + 15.25 + ',' + incline + ',' + incline + '/' + mapWidth + 'x' + mapHeigth + '?access_token=' + accessToken)
+}
+
+function setup() {
+    createCanvas(1024,512);
+    image(imagemMapa, 0,0)
+}
