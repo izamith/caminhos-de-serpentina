fc9bc2ba62bf715d02039018ed21bdcc7003d8f2
Isabela Zamith <izamith@yahoo.com.br>
2021-02-11 17:31:50 -0300

    Criar um objeto para cada bloco e separar coordenadas iniciais em LONGITUDE E LATITUDE

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ piscinadepixel, Isabela Zamith */
 let blocos = []
 let rotaInicio = []
 let rotaFim = []
+let table
+
+
 
 console.log('javascript ok')
 
@@ -27,13 +30,14 @@ async function getData() {
     //como tá gerando os csv
     //.slice "corta" o cabeçalho do array (row[0]), pq
     //coloquei pra começar no 1
-    const table = data.split('\r').slice(1);
-    //pegar cada elemento(elt) de cada linha(row)
-    //loop
+    table = data.split('\r').slice(1);
 
     //debug
         //console.log(table);
+    dataCollection()
+}
 
+function dataCollection(){
     //pegar cada elemento(elt) de cada linha(row)
     //loop
     table.forEach(row=> {
@@ -54,13 +58,59 @@ async function getData() {
        
         //debug
             //console.log(blocos,rotaInicio,rotaFim);
+           
         });
 
+    //passar por todos os blocos
     for (var i = 0; i<blocos.length; i++) {
-        console.log('O bloco '+ blocos[i]+ ' sai de ' + rotaInicio[i] + ' e vai até ' + rotaFim[i])
+
+        //formato da rota de inicio e fim no csv está [LONG LAT]
+        //primeiro precisa remover os [] (primeiro e ultimos caracteres)
+        var j = (rotaInicio[i])
+        var resultInicial = j.substring(1, j.length-1);
+
+        //DIVIDIR EM DOIS INICIAL
+        //divide no espaço e coloca no array fields
+        var fields = resultInicial.split(' ');
+        var long = fields[0];
+        var lat = fields[1];
+
+            //console.log('LATITUDE IS '+ lat + ' AND LONGITUDE IS '+ long)
+
+        var k = (rotaFim[i])
+        var resultFinal 
+            //alguns não tem final e ficam undefined na tabela
+            if (typeof k !=='undefined') {
+                 resultFinal = k.substring(1, k.length-1);
+            }
+        
+        //DIVIDIR EM DOIS FINAL
+        
+    /**********************************************************/    
+            //PAREI AQUI 
+            //DIVIDIR O RESULTADO FINAL EM DOIS
+            //DEPOIS COLOCAR NO OBJETO BLOCO 
+    /**********************************************************/        
+
+
+
+        //cada bloco é um objeto com nome e coordenadas iniciais e finais
+        var bloco = new Bloco(blocos[i])
+            //console.log(bloco)
+       
+        //console.log('O bloco '+ blocos[i]+ ' sai de ' + rotaInicio[i] + ' e vai até ' + rotaFim[i])
     }
 }
 
+
+//funcao construtora de objetos BLOCO
+function Bloco(nome, longInicial, latInicial, longFinal, latFinal) {
+    this.nome = nome;
+    this.longInicial = longInicial;
+    this.latInicial = latInicial;
+    this.longFinal = longFinal;
+    this.latFinal = latFinal;
+  }
 
 
 
