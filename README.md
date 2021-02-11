d169e08e22b3bd4ef129d4e3a268547598bc3895
Isabela Zamith <izamith@yahoo.com.br>
2021-02-11 16:40:57 -0300

    Armazenamento de cada bloco em uma variável (nome, inicio e fim da rota)

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Reconstruindo os caminhos do carnaval de rua carioca pré-pandemia com serpentin
 
 ###### Obtenção de <a href='https://github.com/izamith/caminhos-de-serpentina/blob/main/81d05b_f4ba112f5af24879921317ccbd954ebb.pdf'>dados oficiais</a> sobre os blocos da cidade ✅ <br> [escolhidos aqueles com público acima de 10000 pessoas] 
 
-###### Normalização dos dados oficiais <br> -<a href='https://github.com/izamith/caminhos-de-serpentina/blob/main/10mil.csv'>Transformar em CSV</a> ✅ <br> -Definir longitude e latitude do ponto inicial e final de cada bloco
+###### Normalização dos dados oficiais <br> -<a href='https://github.com/izamith/caminhos-de-serpentina/blob/main/10mil.csv'>Transformar em CSV</a> ✅ <br> -Definir longitude e latitude do ponto inicial e final de cada bloco ✅
  
 ###### Definir coordenadas para teste inicial ✅ <br>[//-22.912791466947173, -43.180046626022985 START //-22.908760525072285, -43.187279242122656 END]
 
--- main.js
+++ main.js
@@ -1,17 +1,70 @@
-var imagemMapa
-let lat= -122.4241
-let long = 37.78
-let incline = 0 
-let mapHeigth = 512
-let mapWidth = 1024
-let accessToken = 'pk.eyJ1IjoicGlzY2luYWRlcGl4ZWwiLCJhIjoiY2trenk1ZzE2MGViYTJ1cG5hbXY1c3A5ZCJ9.lso-cNpB8Id_MW1s6_BM7A'
-let zoom = 15 
+/* caminhos de serpentina, 2021
+piscinadepixel, Isabela Zamith */
 
-function preload() {
-    imagemMapa = loadImage('https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/' + lat + ',' + long + ',' + zoom + ',' + incline + ',' + incline + '/' + mapWidth + 'x' + mapHeigth + '?access_token=' + accessToken)
+//variáveis globais
+//meu token mapbox
+
+
+//armazenamento de dados da tabela
+let blocos = []
+let rotaInicio = []
+let rotaFim = []
+
+console.log('javascript ok')
+
+
+//carrega os dados em variaveis
+async function getData() {
+    //carrega a tabela do arquivo csv
+    const response = await fetch('docs/data.csv');
+    //nome,inicio,fim
+    const data = await response.text();
+    //debug
+        //console.log(data);
+
+    //pega cada linha da tabela até onde acaba (enter), pode ser
+    //'\n' ou '\r', vai depender da tabela, checar antes
+    //como tá gerando os csv
+    //.slice "corta" o cabeçalho do array (row[0]), pq
+    //coloquei pra começar no 1
+    const table = data.split('\r').slice(1);
+    //pegar cada elemento(elt) de cada linha(row)
+    //loop
+
+    //debug
+        //console.log(table);
+
+    //pegar cada elemento(elt) de cada linha(row)
+    //loop
+    table.forEach(row=> {
+        //pega cada linha até onde vai ","
+        //coluna
+        const columns = row.split(',');
+        //elemento 0 da linha
+        const nomeBloco = columns[0];
+        //coloca cada area numa array "xareas"
+        blocos.push(nomeBloco);
+
+        //elemento 1 da linha
+        const inicioBloco = columns[1];
+        rotaInicio.push(inicioBloco);
+        // ...
+        const fimBloco = columns[2];
+        rotaFim.push(fimBloco);
+       
+        //debug
+            //console.log(blocos,rotaInicio,rotaFim);
+        });
+
+    for (var i = 0; i<blocos.length; i++) {
+        console.log('O bloco '+ blocos[i]+ ' sai de ' + rotaInicio[i] + ' e vai até ' + rotaFim[i])
+    }
 }
 
-function setup() {
-    createCanvas(1024,512);
-    image(imagemMapa, 0,0)
+
+
+
+
+async function Start() {
+    await getData();
 }
